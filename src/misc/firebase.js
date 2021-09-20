fbe3973c55061719f06937cdbfcac72e8bfa07d3
varad <varadkhokle10@gmail.com>
2021-09-20 19:06:26 +0530

    first done

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyBf5hecaiOMy2sjNBPDqzemhuJN82Bl6Lo",
+    authDomain: "chat-app-1a2bb.firebaseapp.com",
+    databaseURL: "https://chat-app-1a2bb-default-rtdb.firebaseio.com",
+    projectId: "chat-app-1a2bb",
+    storageBucket: "chat-app-1a2bb.appspot.com",
+    messagingSenderId: "432557440274",
+    appId: "1:432557440274:web:f1805e48e5c086432007cb",
+    measurementId: "G-E355FRHEER"
+  };
+  
+  const app=firebase.initializeApp(config);
