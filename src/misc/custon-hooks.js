70ebc706b5bda71e5dddb4dcc65255f5e1cb3d4c
varad <varadkhokle10@gmail.com>
2021-10-20 20:01:24 +0530

    created layout form home

--- src/misc/custon-hooks.js
+++ src/misc/custon-hooks.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState ,useEffect} from "react";
 
 export function useModelState(defaultValue=false){
     const [isOpen,setIsOpen]=useState(defaultValue);
@@ -9,3 +9,31 @@ export function useModelState(defaultValue=false){
     return {isOpen,open,close};
 
 }
+export const useMediaQuery = query => {
+    console.log("useMediaQuery called ")
+    const [matches, setMatches] = useState(
+      () => window.matchMedia(query).matches
+    );
+  
+    useEffect(() => {
+      const queryList = window.matchMedia(query);
+      console.log("hey i am query list");
+      console.log(queryList);
+      setMatches(queryList.matches);
+  
+      const listener = (evt) =>{
+  
+        console.log("hey i am event")
+         console.log(evt);
+        setMatches(evt.matches);
+      } 
+  
+      queryList.addListener(listener);
+      return () =>{
+        console.log("cleanup function called")
+         queryList.removeListener(listener);
+      }
+    }, [query]);
+  
+    return matches;
+  };
